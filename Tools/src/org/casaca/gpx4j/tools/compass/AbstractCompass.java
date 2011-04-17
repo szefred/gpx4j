ddc501d8d17eb791b17fb7c22a0be3355f5dfde2
superkas83@gmail.com <superkas83@gmail.com@34fea950-5801-e8aa-c058-e69217fd87fc>
2011-04-17 21:20:10 +0000

    - Add extensible objects to persist using extensions. New classes: IExtensible, AbstractExtensible, StringExtensible - New method in GpxDriver to create a IWriter from a classname - Modified jdom reader and writer to use the new persistence using the extensions - Deleted interfaces IMeasurementUnit and ISpeed - New tool: Bearing, determines the bearing of two coordinates and store the cardinal direction of the bearing

--- Tools/src/org/casaca/gpx4j/tools/compass/AbstractCompass.java
+++ Tools/src/org/casaca/gpx4j/tools/compass/AbstractCompass.java
@@ -5,7 +5,7 @@ import java.util.Properties;
 
 import org.casaca.gpx4j.core.data.CoordinatesObject;
 import org.casaca.gpx4j.tools.Tool;
-import org.casaca.gpx4j.tools.data.IMeasurementUnit;
+import org.casaca.gpx4j.tools.data.CardinalDirection;
 import org.casaca.gpx4j.tools.data.MeasurementUnit;
 import org.casaca.gpx4j.tools.util.Constants;
 
@@ -16,7 +16,7 @@ public class AbstractCompass extends Tool implements ICompass {
 	}
 
 	@Override
-	public IMeasurementUnit getUnit() {
+	public MeasurementUnit getUnit() {
 		return MeasurementUnit.DEGREES;
 	}
 
--- Tools/src/org/casaca/gpx4j/tools/compass/ICompass.java
+++ Tools/src/org/casaca/gpx4j/tools/compass/ICompass.java
@@ -3,11 +3,12 @@ package org.casaca.gpx4j.tools.compass;
 import java.math.BigDecimal;
 
 import org.casaca.gpx4j.core.data.CoordinatesObject;
-import org.casaca.gpx4j.tools.data.IMeasurementUnit;
+import org.casaca.gpx4j.tools.data.CardinalDirection;
+import org.casaca.gpx4j.tools.data.MeasurementUnit;
 
 public interface ICompass {
 	
-	public IMeasurementUnit getUnit();
+	public MeasurementUnit getUnit();
 	
 	public <T extends CoordinatesObject> BigDecimal getBearing(T c1, T c2);
 	
